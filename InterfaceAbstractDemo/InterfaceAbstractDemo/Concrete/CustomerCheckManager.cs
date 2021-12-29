535b2b9950bcdac2efdeebda9bc3384421e69a40
Murat Altunkaya <92179456+Murat-ALTUNKAYA@users.noreply.github.com>
2021-12-29 17:41:14 +0300

    FirstCommit

---
+++ InterfaceAbstractDemo/InterfaceAbstractDemo/Concrete/CustomerCheckManager.cs
@@ -0,0 +1,21 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using InterfaceAbstractDemo.MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo.Concrete
+{
+    public class CustomerCheckManager : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            //MernisServiceReference.KPSPublicSoapClient client = new KPSPublicSoapClient();
+            //Bunu kullanmıyoruz çünkü internet sitesinde sıkıntı olduğu zaman bizim sistemimiz de çalışmaz.
+            return true;
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer
+            {
+                DateOfBirth = new System.DateTime(1985,1,6),
+                FirstName="Engin",
+                LastName="Demiroğ",
+                NationalityId="28861499108"
+            });
+            Console.ReadLine();
+        }
+    }
+}
